333c705608749b08683623cc2a82aa5a312039a3
Shrey vats <techtechnicalshrey@gmail.com>
2025-06-27 20:54:05 +0530

    add verify otp controller

---
+++ server/controllers/verifyOTPController .js
@@ -0,0 +1,28 @@
+import User from "../models/user"
+
+export const verifyOTPController = async (req, res) => {
+    const {email, otp} = req.body
+
+    try {
+        const user = User.findOne({ email });
+
+        if (
+          !user ||
+          user.otp !== otp ||
+          new Date(user.otpExpires) < new Date()
+        ) {
+          return res.status(400).json({ message: "Invalid or expired OTP" });
+        }
+
+        user.otp = null;
+        user.otpExpires = null;
+        await user.save();
+
+        res.status(200).json({ message: "OTP verified successfully" });
+    } catch (error) {
+        res
+          .status(500)
+          .json({ message: "OTP verification failed", error: error.message });
+    }
+
+}
